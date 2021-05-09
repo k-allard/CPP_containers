58b2dedc309bff21aa26bf12425d79e96785efb1
kallard <kallard@student.21-school.ru>
2021-05-09 23:07:33 +0300

    new tests and files

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include "vector.hpp"
-// #include "list.hpp"
+#include "ring.hpp"
 
 #include <vector>
 #include <iostream>
@@ -16,5 +16,19 @@ int main()
 
     ft::vector<int> vecMy;
 
+
+    Ring<string> rs;
+    rs.push_back("one");
+    rs.push_back("two");
+    rs.push_back("three");
+    rs.push_back("four");
+    rs.push_back("five");
+    Ring<string>::iterator it = rs.begin();
+    ++it; ++it;
+    it.insert("six");
+    it = rs.begin();
+    // Twice around the ring:
+    for(int i = 0; i < rs.size() * 2; i++)
+    cout << *it++ << endl;
     return 0;
 }
---
+++ map.hpp
@@ -0,0 +1,24 @@
+// sorted associative container that contains key-value pairs with unique keys
+
+#ifndef MAP_HPP
+# define MAP_HPP
+
+#include "header.hpp"
+#include <iostream>
+#include <map>
+
+namespace ft
+{
+template <class Key,
+          class T,
+		  class Compare =  std::less<Key>,
+          class Allocator = std::allocator<std::pair<const Key,T> >
+
+class map
+{
+
+}
+
+}
+
+#endif
