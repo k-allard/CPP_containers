44bc55266ac97bb1515c76d39bba27cfcec9d295
kallard <kallard@student.21-school.ru>
2021-05-13 22:39:01 +0300

    add common includes to header.hpp

--- header.hpp
+++ header.hpp
@@ -1,2 +1,13 @@
 #include <iostream>
 
+// A DLL node
+template <typename T>
+struct node
+{
+	T data;
+	struct node *prev;
+	struct node *next;
+
+	node() : data(T()) {}
+	node(const T &value) : data(value) {}
+};
