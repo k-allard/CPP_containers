4da7c4fdb0db24f9ebc207aadca68b5819c60ec1
kallard <kallard@student.21-school.ru>
2021-05-14 23:59:53 +0300

    add vector member functions

--- header.hpp
+++ header.hpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdexcept>
+#include <limits>
 
 // A DLL 
 template <typename T>
--- vector.hpp
+++ vector.hpp
@@ -16,15 +16,15 @@ namespace ft
     template <class T, class Allocator = std::allocator<T> >
     class vector
     {
-	private: 
+	private:
 		Allocator							_allocator;
 		Allocator::pointer					_ptr;
 		Allocator::size_type				_size;
 		Allocator::size_type				_capacity;
 
     public:
-        typedef T											value_type;               
-        typedef Allocator									allocator_type;      
+        typedef T											value_type;
+        typedef Allocator									allocator_type;
         typedef typename allocator_type::reference			reference;
         typedef typename allocator_type::size_type			size_type;
         typedef typename allocator_type::difference_type	difference_type;
@@ -37,7 +37,7 @@ namespace ft
 		typedef revVectorIterator<T>						reverse_iterator;
 		typedef constRevVectorIterator<T>					const_reverse_iterator;
 
-        // 
+        //
         //  C O N S T R U C T O R S  &  D E S T R U C T O R
         //
 
@@ -48,7 +48,7 @@ namespace ft
         ~vector();
 
 
-        // 
+        //
         //  A S S I G N A T I O N
         //
 
@@ -56,7 +56,7 @@ namespace ft
         template <class InputIterator> void assign(InputIterator first, InputIterator last);    // range
         void assign (size_type n, const value_type& val);   // fill
 
-        // 
+        //
         //  I T E R A T O R S
         //
 
@@ -69,9 +69,9 @@ namespace ft
         reverse_iterator rend();
         const_reverse_iterator rend() const;
 
-        // 
+        //
         //  C A P A C I T Y
-        // 
+        //
 
         size_type size() const;
         size_type max_size() const;
@@ -80,7 +80,7 @@ namespace ft
         void reserve (size_type n);
         void resize (size_type n, value_type val = value_type());
 
-        // 
+        //
         //  E L E M E N T   A C C E S S
         //
 
@@ -109,21 +109,21 @@ namespace ft
 
     };
 
-	// 
+	//
 	//  I T E R A T O R S
 	//
 
-	template <class T, class Allocator> 
+	template <class T, class Allocator>
 	typename vector<T, Allocator>::iterator vector<T, Allocator>::begin() {
 		return (iterator(_ptr));
 	}
 
-	template <class T, class Allocator> 
+	template <class T, class Allocator>
 	typename vector<T, Allocator>::const_iterator vector<T, Allocator>::begin() const {
 		return (const_iterator(_ptr));
 	}
 
-	template <class T, class Allocator> 
+	template <class T, class Allocator>
 	typename vector<T, Allocator>::iterator vector<T, Allocator>::end() {
 		return (iterator(&(_ptr[_size])));
 	}
@@ -132,7 +132,7 @@ namespace ft
 	typename vector<T, Allocator>::const_iterator vector<T, Allocator>::end() const {
 		return (const_iterator(&(_ptr[_size])));
 	}
-	
+
 	template <class T, class Allocator>
 	typename vector<T, Allocator>::reverse_iterator vector<T, Allocator>::rbegin() {
 		return (reverse_iterator(&(_ptr[_size - 1])));
@@ -147,13 +147,56 @@ namespace ft
 	typename vector<T, Allocator>::reverse_iterator vector<T, Allocator>::rend() {
 		return (reverse_iterator(_ptr - 1));
 	}
-	
+
 	template <class T, class Allocator>
 	typename vector<T, Allocator>::const_reverse_iterator vector<T, Allocator>::rend() const {
 		return (const_reverse_iterator(_ptr - 1));
 	}
 
-	// 
+	//
+	//  C A P A C I T Y
+	//
+
+	template <class T, class Allocator>
+	typename vector<T, Allocator>::size_type vector<T, Allocator>::size() const {
+		return (_size);
+	}
+
+	template <class T, class Allocator>
+	typename vector<T, Allocator>::size_type vector<T, Allocator>::max_size() const {
+		return (std::numeric_limits<size_type>::max() / sizeof(value_type));
+	}
+
+	template <class T, class Allocator>
+	typename vector<T, Allocator>::size_type vector<T, Allocator>::capacity() const {
+		return (_capacity);
+	}
+
+	template <class T, class Allocator>
+	bool vector<T, Allocator>::empty() const {
+		return (_size_ == 0);
+	}
+
+	template <class T, class Allocator>
+	void vector<T, Allocator>::reserve (size_type n) {
+		if (n > _capacity) {
+			T* temp = _allocator.allocate(n);
+			for (size_type i = 0; i < _size; i++) {
+				_allocator.construct(temp + i, _ptr[i]);
+				_allocator.destroy(_ptr + i);
+			}
+			_alloc.deallocate(_ptr, _capacity);
+			_capacity = n;
+			_ptr = temp;
+		}
+	}
+
+	template <class T, class Allocator>
+	void vector<T, Allocator>::resize (size_type n, value_type val = value_type()) {
+
+	}
+
+	//
 	//  E L E M E N T   A C C E S S
 	//
 
@@ -201,11 +244,11 @@ namespace ft
 		return (_ptr[_size - 1]);
 	}
 
-	// 
+	//
 	//  N O N - M E M B E R S
 	//
 
-	template <class T, class Alloc> 
+	template <class T, class Alloc>
 	void swap (vector<T,Alloc>& x, vector<T,Alloc>& y);
 
 
